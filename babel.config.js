1101e7097dfa7d40e34771fbefb8885915df56f0
YumProjects <32969785+YumProjects@users.noreply.github.com>
2021-03-14 18:22:18 -0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   
     return {
         "presets": [
-            ["@babel/env", { "targets": "> 0.25%, not dead", "useBuiltIns": "usage", "corejs": { "version": "3.9.1", "proposals": true } }],
+            ["@babel/env", { "useBuiltIns": "usage", "corejs": { "version": "3.9.1", "proposals": true } }],
             ["@babel/preset-typescript", { "allExtensions": true, "isTSX": true }],
         ],
         "plugins": [
